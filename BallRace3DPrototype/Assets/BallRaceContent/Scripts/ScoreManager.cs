c8842bf6a43613f05f8013e0c913c73475d89e6a
Dex10k <ddeboone@gmail.com>
2019-10-30 16:36:40 -0400

    Created Score Volumes

---
+++ BallRace3DPrototype/Assets/BallRaceContent/Scripts/ScoreManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    int Player1Score = 0;
+    int Player2Score = 0;
+    int Player3Score = 0;
+    int Player4Score = 0;
+
+    public Text Player1ScoreDisplay;
+    public Text Player2ScoreDisplay;
+    public Text Player3ScoreDisplay;
+    public Text Player4ScoreDisplay;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateUIElements();
+    }
+
+    public void addScore(int PlayerID, int ScoreIncrease)
+    {
+        if(PlayerID == 0)
+        {
+            Player1Score += ScoreIncrease;
+        }
+        else if (PlayerID == 1)
+        {
+            Player2Score += ScoreIncrease;
+        }
+        else if (PlayerID == 2)
+        {
+            Player3Score += ScoreIncrease;
+        }
+        else if (PlayerID == 3)
+        {
+            Player4Score += ScoreIncrease;
+        }
+    }
+
+
+
+    void UpdateUIElements()
+    {
+        Player1ScoreDisplay.text = ("" +Player1Score);
+        Player2ScoreDisplay.text = ("" + Player2Score);
+        Player3ScoreDisplay.text = ("" + Player3Score);
+        Player4ScoreDisplay.text = ("" + Player4Score);
+    }
+}
---
+++ BallRace3DPrototype/Assets/BallRaceContent/Scripts/ScoreVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreVolume : MonoBehaviour
+{
+    public ScoreManager scoreManagerComponent;
+
+    public int VolumeScoreWorth;
+
+    public int Cooldown;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            int PlayerID = other.gameObject.GetComponent<PlayerMovement>().playerID;
+
+            scoreManagerComponent.addScore(PlayerID, VolumeScoreWorth);
+        }
+    }
+
+
+ 
+}
