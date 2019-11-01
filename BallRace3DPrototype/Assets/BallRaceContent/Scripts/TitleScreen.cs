cc661aa3b2e4aa4f6c56abb1bc570ec34a13f5db
Dex10k <ddeboone@gmail.com>
2019-10-31 23:30:59 -0400

    TitleScreen Setup Complete

---
+++ BallRace3DPrototype/Assets/BallRaceContent/Scripts/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            LoadGame();
+        } else if (Input.GetButtonDown("Cancel"))
+        {
+            QuitGame();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Application");
+       Application.Quit();
+    }
+}
